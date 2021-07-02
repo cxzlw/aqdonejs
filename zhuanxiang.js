f98112b1796b84423b2b58cf1912f7cc215b3d08
cxzlw <55052188+cxzlw@users.noreply.github.com>
2021-07-02 22:47:34 +0800

    Create zhuanxiang.js

---
+++ zhuanxiang.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         安全教育平台专项作业一键完成脚本 by cxzlw
+// @namespace    https://github.com/cxzlw
+// @version      0.01a
+// @description  由创新者老王制作用于在点击问题回答提交时破解问题校验程序的小脚本
+// @author       https://github.com/cxzlw
+// @match        https://huodong.xueanquan.com/*/*.html*
+// @icon         chrome://favicon/http://huodong.xueanquan.com/
+// @grant        none
+// ==/UserScript==
+/*
+本程序由创新者老王制作,发布于https://github.com/cxzlw/aqdonejs/
+程序名:zhuanxiang.js
+*/
+
+(function() {
+    'use strict';
+    console.log("安全教育平台专项作业一键完成脚本 by cxzlw: 脚本已加载");
+    questionMust = function(){console.log("安全教育平台专项作业一键完成脚本 by cxzlw: 已成功拦截了一次完整性检测");return true;}
+})();
