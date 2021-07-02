346e0d8e59b9e7c7f8083c2ff03a399b16a99e08
cxzlw <55052188+cxzlw@users.noreply.github.com>
2021-07-02 22:47:52 +0800

    Create putong.js

---
+++ putong.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         安全教育平台普通作业一键完成脚本 by cxzlw
+// @namespace    https://github.com/cxzlw
+// @version      0.01a
+// @description  由创新者老王制作用于在点击问题回答提交时破解问题校验程序的小脚本
+// @author       https://github.com/cxzlw
+// @match        https://*.xueanquan.com/JiaTing/EscapeSkill/SeeVideo.aspx*
+// @icon         chrome://favicon/http://xueanquan.com/
+// @grant        none
+// ==/UserScript==
+/*
+本程序由创新者老王制作,发布于https://github.com/cxzlw/aqdonejs/
+程序名:putong.js
+*/
+
+(function() {
+    'use strict';
+
+    console.log("安全教育平台普通作业一键完成脚本 by cxzlw: 脚本已加载");
+    $('#input_button').click(function () {
+        console.log('安全教育平台普通作业一键完成脚本 by cxzlw: 成功的拦截了问题校验,祝你有一个美好的未来😊');
+        threeTest = 0;
+    });
+})();
